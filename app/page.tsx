7a359c9ddb954f833058f2d7d80d4881f5699ee8
naman-rathore99 <157107543+naman-rathore99@users.noreply.github.com>
2024-06-26 11:26:21 +0000

    sidebar fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Navbar from "@/components/Navbar";
-import Sidebar, { Sidebaritems } from "@/components/Sidebar"
+import Sidebar, {SidebarItem} from "@/components/Sidebar"
 import { Boxes, LayoutDashboard, Package, Receipt, Settings, UserCircle } from "lucide-react";
 
 export default function Home() {
@@ -12,12 +12,12 @@ export default function Home() {
     <div className="flex h-screen">
       <div className="flex flex-col ">
         <Sidebar>
-          <Sidebaritems icon={<LayoutDashboard size={20} />} text="Dashboard" alert active={undefined} />
-          <Sidebaritems icon={<UserCircle size={20} />} text="Dashboard" alert />
-          <Sidebaritems icon={<Boxes size={20} />} text="Dashboard" alert />
-          <Sidebaritems icon={<Package size={20} />} text="Dashboard" alert />
-          <Sidebaritems icon={<Receipt size={20} />} text="Dashboard" alert />
-          <Sidebaritems icon={<Settings size={20} />} text="Dashboard" alert />
+          <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard" alert active={undefined} />
+          <SidebarItem icon={<UserCircle size={20} />} text="Dashboard" alert />
+          <SidebarItem icon={<Boxes size={20} />} text="Dashboard" alert />
+          <SidebarItem icon={<Package size={20} />} text="Dashboard" alert />
+          <SidebarItem icon={<Receipt size={20} />} text="Dashboard" alert />
+          <SidebarItem icon={<Settings size={20} />} text="Dashboard" alert />
         </Sidebar>
       </div>
       <Navbar title="Music" />
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,46 +1,103 @@
-import { ChevronFirst, MoreVertical } from "lucide-react"
+"use client"
+import { MoreVertical, ChevronLast, ChevronFirst } from "lucide-react"
+import { useContext, createContext, useState } from "react"
 
-const Sidebar = ({ children }: any) => {
-    return (
+const SidebarContext = createContext()
 
-        <aside className="h-screen">
-            <nav className="h-full flex flex-col bg-white border-r shadow-sm">
-                <div className="p-4 pb-2 flex flex-between items-center">
-                    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Ffreebiesupply.com%2Flogos%2Ftemp-logo-2%2F&psig=AOvVaw0AJxcH7WSkvqJy1N1Rzzxr&ust=1719301334413000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCMjQu97e84YDFQAAAAAdAAAAABAE" alt="" className="w-32" />
-                    <button className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100">
-                        <ChevronFirst />
-                    </button>
-                </div>
-                <ul className="flex-1 px-3">{children}</ul>
-                <div className="border-t flex p-3">
-                    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Ffreebiesupply.com%2Flogos%2Ftemp-logo-2%2F&psig=AOvVaw0AJxcH7WSkvqJy1N1Rzzxr&ust=1719301334413000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCMjQu97e84YDFQAAAAAdAAAAABAE" alt="" className="w-10 h-10 rounded-md" />
+export default function Sidebar({ children }) {
+  const [expanded, setExpanded] = useState(true)
+  
+  return (
+    <aside className="h-screen">
+      <nav className="h-full flex flex-col bg-white dark:bg-black  border-r shadow-sm">
+        <div className="p-4 pb-2 flex justify-between items-center ">
+          <img
+            src="https://img.logoipsum.com/243.svg"
+            className={`overflow-hidden transition-all ${
+              expanded ? "w-32" : "w-0"
+            }`}
+            alt=""
+          />
+          <button
+            onClick={() => setExpanded((curr) => !curr)}
+            className="p-1.5 rounded-lg bg-gray-50 dark:bg-purple-400  hover:bg-gray-100"
+          >
+            {expanded ? <ChevronFirst /> : <ChevronLast />}
+          </button>
+        </div>
 
-                    <div className="flex justify-between items-center w-52 ml-3"></div>
-                    <div>
-                        <h4 className="font-semibold">Sidebar</h4>
-                        <span className="text-xs text-gray-600">text</span>
-                    </div>
-                    <MoreVertical size={20} />
-                </div>
-            </nav>
-        </aside>
-    )
+        <SidebarContext.Provider value={{ expanded }}>
+          <ul className="flex-1 px-3">{children}</ul>
+        </SidebarContext.Provider>
+
+        <div className="border-t flex p-3">
+          <img
+            src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true"
+            alt=""
+            className="w-10 h-10 rounded-md"
+          />
+          <div
+            className={`
+              flex justify-between items-center
+              overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}
+          `}
+          >
+            <div className="leading-4">
+              <h4 className="font-semibold">John Doe</h4>
+              <span className="text-xs text-gray-600">johndoe@gmail.com</span>
+            </div>
+            <MoreVertical size={20} />
+          </div>
+        </div>
+      </nav>
+    </aside>
+  )
 }
 
-export default Sidebar;
+export function SidebarItem({ icon, text, active, alert }:any) {
+  const { expanded } = useContext(SidebarContext)
+  
+  return (
+    <li
+      className={`
+        relative flex items-center py-2 px-3 my-1
+        font-medium rounded-md cursor-pointer
+        transition-colors group
+        ${
+          active
+            ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
+            : "hover:bg-indigo-50 text-gray-600 dark:text-white dark:hover:text-black"
+        }
+    `}
+    >
+      {icon}
+      <span
+        className={`overflow-hidden transition-all ${
+          expanded ? "w-52 ml-3" : "w-0"
+        }`}
+      >
+        {text}
+      </span>
+      {alert && (
+        <div
+          className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${
+            expanded ? "" : "top-2"
+          }`}
+        />
+      )}
 
-export function Sidebaritems({ icon, text, active, alert }: {
-    icon: any;
-    text: any;
-    active: any;
-    alert: any;
-    size: number;
-}): JSX.Element {
-    return (
-        <li className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors ${active ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800": "hover:bg-indigo-50 text-gray-600"}`}>
-            {icon}
-            <span className="w-52 ml-3">{text}</span>
-            {alert && <div className={"absolute right-2 w-2 h-2 rounded bg-indigo-400"  }/>}
-        </li>
-    )
+      {!expanded && (
+        <div
+          className={`
+          absolute left-full rounded-md px-2 py-1 ml-6
+          bg-indigo-100 text-indigo-800 text-sm
+          invisible opacity-20 -translate-x-3 transition-all
+          group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
+      `}
+        >
+          {text}
+        </div>
+      )}
+    </li>
+  )
 }
